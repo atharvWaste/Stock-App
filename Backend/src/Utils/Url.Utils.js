d31ee1fef93564e98207e88efba5a1d705c1e1f6
atharvWaste <atharvawaste16@gmail.com>
2026-03-24 22:55:55 +0530

    Some thing fuckend up

--- Backend/src/Utils/Url.Utils.js
+++ Backend/src/Utils/Url.Utils.js
@@ -1,10 +1,8 @@
-const Url = ()=>{
-return[
-    `https://stock.indianapi.in/fetch_52_week_high_low_data`,
-`https://stock.indianapi.in/BSE_most_active`,
- `https://stock.indianapi.in/NSE_most_active`,
- `https://stock.indianapi.in/trending`,
- ];
+const Url = () => [
+  "https://stock.indianapi.in/fetch_52_week_high_low_data",
+  "https://stock.indianapi.in/BSE_most_active",
+  "https://stock.indianapi.in/NSE_most_active",
+  "https://stock.indianapi.in/trending",
+];
 
-}
-export default Url
+export default Url;
